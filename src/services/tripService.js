7083819193d1e2ac5939572f9638f9419a2b574a
Mayara Milaneesi <mayaramilanesim@gmail.com>
2024-10-15 22:46:35 -0300

    fixing API_URL

--- src/services/tripService.js
+++ src/services/tripService.js
@@ -1,13 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL
+let API_URL = process.env.VITE_API_URL;
+
+if (API_URL === "") {
+  API_URL = import.meta.env.VITE_API_URL;
+}
+
+console.log("LOG - API_URL", API_URL);
 
 export const fetchTrips = async (page, limit = 10) => {
   try {
     const response = await axios.get(`${API_URL}?page=${page}&limit=${limit}`);
     return response.data;
   } catch (error) {
-    console.error('Erro ao buscar viagens', error);
+    console.error("Erro ao buscar viagens", error);
     throw error;
   }
 };
